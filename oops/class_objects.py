6f944d70da266d587a09e405b39577e70a155232
pappu <pappuyadav98199@gmail.com>
2026-05-03 16:16:47 +0545

    Default and Alternative constructor added

--- oops/class_objects.py
+++ oops/class_objects.py
@@ -1,4 +1,4 @@
-#project 1
+#class and objects
 class BankAccount:
     def __init__(self,name,balance=0):
         self.name=name
@@ -17,3 +17,38 @@ class BankAccount:
         
 obj1=BankAccount('ram',1000)
 obj1.withdraw(5000)
+
+#constructor(init)
+class Customer:
+    def __init__(self,name,account_type): #default constructor
+        print('Default constructor is called')
+        self.name=name
+        self.account_type=account_type
+        print(f'name:{name} and account_types:{account_type}')
+    @classmethod #alternative constructor
+    def customer_details(cls,name,account_type):
+        print('Alternative constructor is called')
+        return cls(name,account_type)
+    @classmethod
+    def get_name_account_details(cls,data):
+        print('Alternative Constructor is called')
+        name,account_details=data.split('-')
+        return cls(name,account_details) 
+    @classmethod
+    def get_dict_data(cls,dict_data):
+        print('Alternative Constructor is called')
+        name=dict_data.get('name')
+        account_type=dict_data.get('account_type')
+        return cls(name,account_type) 
+        
+obj2=Customer('ram','saving')
+#Alternative constructor :customer details
+obj3=Customer.customer_details('shyam','saving')
+
+#note:while calling Alternative constructor the data from Alternative constructor is passed to Default constructor
+
+#Alternative constructor :get_name_account_details
+obj4=Customer.get_name_account_details('pappu-saving')
+
+#Alternative constructor :get_dict_data
+obj5=Customer.get_dict_data({'name':'Hari','account_type':'Saving'})
