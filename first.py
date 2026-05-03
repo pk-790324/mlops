afb079b72d4047bda8f01c51293d3c1dd269e0a0
pappu <pappuyadav98199@gmail.com>
2026-05-03 14:55:35 +0545

    all

--- first.py
+++
@@ -1,5 +0,0 @@
-st='welcome to the mlops course'
-a='this is a beginners course'
-last='this is not the end of the course'
-print(st)
-print(a)
--- helper2.py
+++
@@ -1,5 +0,0 @@
-sts='helper 2 message'
-print(sts)
-
-
-sts='this is from master bracch'
